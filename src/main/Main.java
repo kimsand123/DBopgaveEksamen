029fccd21d5ef8e420743860fbb8c70c59ae21d0
Frederik Martin <frederik@mybid.dk>
2018-05-03 17:23:22 +0200

    Revert "Added main to MySqlReceptDAO"
    This reverts commit 156182c2e24d203960cfdb557854ff572d1ea5b4.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,5 +3,7 @@ package main;
 import testCases.UpdateReceptTest;
 
 public class Main {
-	
+	public static void main(String[] args) {
+		new UpdateReceptTest().Run(1);
+	}
 }
--- src/testCases/UpdateReceptTest.java
+++ src/testCases/UpdateReceptTest.java
@@ -8,10 +8,6 @@ import tools.KeyValue;
 
 public class UpdateReceptTest {
 
-	public static void main(String[] args) {
-		new UpdateReceptTest().Run(1);
-	}
-	
 	public void Run(int receptId) {
 		ReceptController recCtrl = new ReceptController();
 		ReceptDTO recept = recCtrl.getRecept(receptId);
