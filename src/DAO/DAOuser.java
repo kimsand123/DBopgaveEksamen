d2e1b288513d22d230e18e47e485be558dd37765
Kasper Valente <savethecat@gmail.com>
2018-05-03 17:10:19 +0200

    Added update user to DAOUser.java

--- src/DAO/DAOuser.java
+++ src/DAO/DAOuser.java
@@ -13,7 +13,7 @@ import java.sql.*;
 public class DAOuser {
 	private Connection createConn()
 	{
-		
+
 		Connection conn = null;
 		try {
 			conn = DriverManager.getConnection("jdbc:mysql://mysql3.unoeuro.com:3306/nybaad_dk_db2", "nybaad_dk", "rgkd49cz");
@@ -21,71 +21,105 @@ public class DAOuser {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return conn;
-		
+
 	}
-	
-	
-	
+
+
+
 	public void createUser( UserDTO userDTO) {
-		
-						
-			 
+
 		Connection conn = createConn();
-		
-		
-	CallableStatement castm = null;
-	try {
-		castm = conn.prepareCall("{call sp_create_medarbejder(?,?,?,?,?,?,?,?,?,?)}");
-		castm.setInt(1, userDTO.getUser_id());
-		castm.setString(2, userDTO.getUser_fornavn());
-		castm.setString(3, userDTO.getUser_efternavn());
-		castm.setString(4, userDTO.getUser_ini());
-		castm.setString(5, userDTO.getUser_cpr());
-		castm.setString(6, userDTO.getUser_password());
-	
-		for(int x = 0; x < userDTO.getRoles().size(); x++)
-		{
-		String hell =	userDTO.getRoles().get(x);
-		switch(hell) {
-		
-		case "Administrator":
-		castm.setInt(7, 1);
-		case "Foreman":
-		castm.setInt(8, 1);
-		case"Master_Chef":
-		castm.setInt(9, 1);
-		case"Operatoer":
-		castm.setInt(10, 1);
+		CallableStatement castm = null;
+		try {
+			castm = conn.prepareCall("{call sp_create_medarbejder(?,?,?,?,?,?,?,?,?,?)}");
+			castm.setInt(1, userDTO.getUser_id());
+			castm.setString(2, userDTO.getUser_fornavn());
+			castm.setString(3, userDTO.getUser_efternavn());
+			castm.setString(4, userDTO.getUser_ini());
+			castm.setString(5, userDTO.getUser_cpr());
+			castm.setString(6, userDTO.getUser_password());
+
+			for(int x = 0; x < userDTO.getRoles().size(); x++)
+			{
+				String hell =	userDTO.getRoles().get(x);
+				switch(hell) {
+
+				case "Administrator":
+					castm.setInt(7, 1);
+				case "Foreman":
+					castm.setInt(8, 1);
+				case"Master_Chef":
+					castm.setInt(9, 1);
+				case"Operatoer":
+					castm.setInt(10, 1);
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+		try {
+			castm.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+
 	}
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-		
-	try {
-		castm.executeQuery();
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}	
-		
-	}
-	
-	
-	
-	
+
+
+
+
 	public void updateUser(UserDTO userDTO) {
-	
-		
-		
-		
+
+
+
+		Connection conn = createConn();
+		CallableStatement castm = null;
+		try {
+			castm = conn.prepareCall("{call sp_update_medarbejder(?,?,?,?,?,?,?,?,?,?)}");
+			castm.setInt(1, userDTO.getUser_id());
+			castm.setString(2, userDTO.getUser_fornavn());
+			castm.setString(3, userDTO.getUser_efternavn());
+			castm.setString(4, userDTO.getUser_ini());
+			castm.setString(5, userDTO.getUser_cpr());
+			castm.setString(6, userDTO.getUser_password());
+
+			for(int x = 0; x < userDTO.getRoles().size(); x++)
+			{
+				String hell =	userDTO.getRoles().get(x);
+				switch(hell) {
+
+				case "Administrator":
+					castm.setInt(7, 1);
+				case "Foreman":
+					castm.setInt(8, 1);
+				case"Master_Chef":
+					castm.setInt(9, 1);
+				case"Operatoer":
+					castm.setInt(10, 1);
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			castm.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+
+
 	}
-	
-	
-	
+
+
+
 	public void deleteUser(int userId)
 	{
 		Connection conn = createConn();
@@ -93,30 +127,30 @@ public class DAOuser {
 		try {
 			castm = conn.prepareCall("{call sp_delete_medarbejder(?)");
 			castm.setInt(1, userId);
-		
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		try {
 			castm.executeQuery();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	
-		
-		
-		
-		
+
+
+
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
 
 }
