cbd2b6244d18f5285bd1c753529ba78d79479b00
lupuscarnis <32562715+lupuscarnis@users.noreply.github.com>
2018-05-03 14:05:03 +0200

    Pusing hard

--- src/DAO/DAOuser.java
+++ src/DAO/DAOuser.java
@@ -2,10 +2,9 @@ package DAO;
 
 import connector01917.Connector;
 import connector01917.Constant;
-import dto01917.OperatoerDTO;
+import dto01917.UserDTO;
 import DAOinterfaces.*;
 import DTO.ProductNProviderDTO;
-
 import java.sql.*;
 
 
@@ -29,7 +28,7 @@ public class DAOuser {
 	
 	
 	
-	public void createUser( UsaerDTO userDTO) {
+	public void createUser( UserDTO userDTO) {
 		
 						
 			 
